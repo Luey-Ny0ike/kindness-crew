5f7c50ca7f82494383d9d91996b40fa97589d5f4
shariff6 <shariff.mohammed.a@gmail.com>
2017-11-22 18:03:28 +0300

    adds name and number to volunteer registration and edit params

--- app/controllers/volunteer/registrations_controller.rb
+++ app/controllers/volunteer/registrations_controller.rb
@@ -36,13 +36,19 @@ class Volunteer::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
+  def sign_up_params
+    params.require(:volunteer).permit(:first_name, :last_name, :number, :email, :password, :password_confirmation)
+  end
 
+  def account_update_params
+    params.require(:volunteer).permit(:first_name, :last_name, :number, :email, :password, :password_confirmation, :current_password)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
