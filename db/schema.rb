b1f888c12209abe9f010af30efb1775e40bb1b8b
shariff6 <shariff.mohammed.a@gmail.com>
2017-11-22 18:05:36 +0300

    employers table added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119101625) do
+ActiveRecord::Schema.define(version: 20171122135518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "employers", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "number", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_employers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employers_on_reset_password_token", unique: true
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -23,6 +49,9 @@ ActiveRecord::Schema.define(version: 20171119101625) do
   end
 
   create_table "volunteers", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "number", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
