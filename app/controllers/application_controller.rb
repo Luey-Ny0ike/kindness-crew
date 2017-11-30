a4ab62e1623a90b4f210e2726883a40bf51cf0d0
Lewis <nyoikelewis@gmail.com>
2017-11-30 21:51:21 +0300

    Redundant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   # To redirect to dashboard after succesfull log in
   def after_sign_in_path_for(employer)
     @user = current_employer
