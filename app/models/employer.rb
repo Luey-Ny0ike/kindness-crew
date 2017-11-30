d4c2c70c069765da7fef6bac074caf6c882de5bc
Lewis <nyoikelewis@gmail.com>
2017-11-30 22:44:51 +0300

    Relation between the two models

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,7 +1,8 @@
 class Employer < ApplicationRecord
+  has_many :jobs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
-         mount_uploader :background, BackgroundUploader  
+         mount_uploader :background, BackgroundUploader
 end
