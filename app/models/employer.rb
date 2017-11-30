1b1fd6cd8b4b3d27be1dba35a59f92f7050dc9e9
lewis nyoike <nyoikelewis@gmail.com>
2017-11-30 15:07:28 +0300

    Mounted the background uploader

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -3,4 +3,5 @@ class Employer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+         mount_uploader :background, BackgroundUploader  
 end
