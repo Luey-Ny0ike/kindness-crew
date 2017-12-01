84da03fe4993ccafc3b275647d46d0998e61fb05
Lewis <nyoikelewis@gmail.com>
2017-12-01 11:30:48 +0300

    Activated the geo coder gem

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ApplicationRecord
   belongs_to :employer
+  # Established relation with the employer
   mount_uploaders :pictures, PictureUploader
+  # Mounted the picture uploaders
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
+  # Activaion for the geocoder gem
 end
