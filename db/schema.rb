eac38ee36bec7b0fb31f09aef0ba2f0a374b0be2
Lewis <nyoikelewis@gmail.com>
2017-12-01 09:53:31 +0300

    migrated the db account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130190336) do
+ActiveRecord::Schema.define(version: 20171201015911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20171130190336) do
     t.datetime "updated_at", null: false
     t.json "pictures"
     t.integer "employer_id"
+    t.time "time"
+    t.date "date"
   end
 
   create_table "volunteers", force: :cascade do |t|
